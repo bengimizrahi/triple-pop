8f6886de6d04785ad5c6eba552436ff5dfdb8382
Bengi Mizrahi <bengimizrahi@gmail.com>
2016-06-04 00:40:02 +0300

    Implement boilarplate Ball class

--- model/model.h
+++ model/model.h
@@ -1,6 +1,7 @@
 #ifndef HEXAGRID_H
 #define HEXAGRID_H
 
+#include <SMFL/Graphics.hpp>
 constexpr float hexagrid_radius = 10.0f;
 constexpr float ball_radius = hexagrid_radius * 0.9;
 enum Direction {NE, N, NW, SW, S, SE};
@@ -9,5 +10,9 @@ constexpr int num_of_hexagrids(int level)
 {
     return (3 * level * level) - (3 * level) + 1
 }
+
+struct Ball {
+    sf::Color color_{};
+};
 
 #endif // HEXAGRID_H
