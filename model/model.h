babb9f229105ecaa148282e98c2abea174a7e095
Bengi Mizrahi <bengimizrahi@gmail.com>
2016-06-04 00:38:06 +0300

    Implement constexpr num_of_hexagrids(level)

--- model/model.h
+++ model/model.h
@@ -5,4 +5,9 @@ constexpr float hexagrid_radius = 10.0f;
 constexpr float ball_radius = hexagrid_radius * 0.9;
 enum Direction {NE, N, NW, SW, S, SE};
 
+constexpr int num_of_hexagrids(int level)
+{
+    return (3 * level * level) - (3 * level) + 1
+}
+
 #endif // HEXAGRID_H
