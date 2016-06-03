96068ecb8b6066098d5447eb144772c49d675e01
Bengi Mizrahi <bengimizrahi@gmail.com>
2016-06-04 00:37:32 +0300

    Implement some model constants

---
+++ model/model.h
@@ -0,0 +1,8 @@
+#ifndef HEXAGRID_H
+#define HEXAGRID_H
+
+constexpr float hexagrid_radius = 10.0f;
+constexpr float ball_radius = hexagrid_radius * 0.9;
+enum Direction {NE, N, NW, SW, S, SE};
+
+#endif // HEXAGRID_H
