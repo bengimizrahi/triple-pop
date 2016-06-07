057b74c9636e3e6443dd4d6f567c3495ec5e13cd
Bengi Mizrahi <bengimizrahi@gmail.com>
2016-06-07 23:06:46 +0300

    Setup neighbor_[] member with correct links

--- model/model.h
+++ model/model.h
@@ -3,6 +3,7 @@
 
 #include <memory>
 #include <vector>
+#include <array>
 
 constexpr float hexagrid_radius = 10.0f;
 constexpr float ball_radius = hexagrid_radius * 0.9;
@@ -18,11 +19,19 @@ constexpr int hexamesh_center_idx(int level)
     { return hexamesh_2d_array_size(level) / 2; }
 
 class Hexagrid {
+public:
+    Hexagrid* neighbor(Direction direction)
+        { return neighbors_[direction]; }
+    const Hexagrid* neighbor(Direction direction) const
+        { return neighbors_[direction]; }
+    void set_neighbor(Direction direction, Hexagrid* neigh)
+        { neighbors_[direction] = neigh; }
 private:
     std::array<Hexagrid*, 6> neighbors_{{}};
 };
 
 class Hexamesh {
+public:
     Hexamesh(int level);
 private:
     std::vector<std::unique_ptr<Hexagrid>> hexagrids_;
